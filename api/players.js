45f3579f82e0d9e1e75ceb4e02aa8aa7e7c4b8d4
nosjh <sonjunhee34@gmail.com>
2026-03-25 14:19:49 +0900

    fix

--- api/players.js
+++ api/players.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { teamId } = req.query;
   const response = await fetch(
-    `https://api.pandascore.co/lol/teams/${teamId}/players`,
+    `https://api.pandascore.co/lol/players?filter[team_id]=${teamId}`,
     { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } }
   );
   const data = await response.json();
