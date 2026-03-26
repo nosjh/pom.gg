d25b0287b9854ab203f2f64cbf63b852587c3307
nosjh <sonjunhee34@gmail.com>
2026-03-26 10:43:21 +0900

    fix

--- api/matches.js
+++ api/matches.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const params = new URLSearchParams(req.query);
   const response = await fetch(
-    `https://api.pandascore.co/lol/matches?${params}`,
+    `https://api.pandascore.co/lol/matches?${params}&filter[status]=finished`,
     { headers: { Authorization: `Bearer ${process.env.REACT_APP_API_KEY}` } }
   );
   const data = await response.json();
